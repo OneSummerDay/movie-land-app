8cef451b3cf5fb504ff9057dee1d4a0d8e199825
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-20 15:37:23 +0300

    added static data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useEffect } from 'react';
 
 import './App.css';
-import SearchIcom from './search.svg'
+import SearchIcon from './search.svg'
 
 // bdceff9f - Api Key
 
 const API_URL = 'http://www.omdbapi.com?apikey=bdceff9f';
+
+const movie1 = {
+  'Title': 'Amazing Spider Man',
+  'Year': '2012',
+  'imdbID': 'tt2586634',
+  'Type': 'movie',
+  'Poster': 'N/A'
+}
 
 
 const App = () => {
@@ -22,7 +30,25 @@ const App = () => {
   }, [])
 
     return (
-      <h1>Hello React</h1>
+      <div className='app'>
+        <h1>MovieLand</h1>
+
+        <div className='search'>
+          <input 
+            placeholder='Search for movies'
+            value='Superman'
+            onChange={() => {}}
+          />
+          <img 
+            src={SearchIcon}
+            alt='search'
+            onClick={() => {}}
+          />
+        </div>
+        <div className='container'>
+
+        </div>
+      </div>
     );
 }
 
