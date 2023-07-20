5c90d0d54566539515db573b0e4da5df91777669
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-20 14:29:30 +0300

    added svg

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ const API_URL = 'http://www.omdbapi.com?apikey=bdceff9f';
 
 const App = () => {
 
-  useEffect = (() => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}$s=${title}`);
+    const data = await response.json();
 
+    console.log(data.Search)
+  }
+
+  useEffect = (() => {
+    searchMovies('Spiderman');
   }, [])
 
     return (
