2e537eae5f4bb39bb96bd68724a854e7ce806fbf
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-20 14:10:24 +0300

    added an amdbapi API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+// bdceff9f - Api Key
+
+const API_URL = 'http://www.omdbapi.com?apikey=bdceff9f';
+
 
 const App = () => {
+
+  useEffect = (() => {
+
+  }, [])
+
     return (
       <h1>Hello React</h1>
     );
