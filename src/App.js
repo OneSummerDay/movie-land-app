f61e745e6d7e341e5a098ac42b79b037f8a27766
OneSummerDay <artem.kraievyi@gmail.com>
2023-07-20 16:19:37 +0300

    added state for searching input

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ const movie1 = {
 const App = () => {
   const [movies, setMovies] = useState([]);
 
-
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}$s=${title}`);
+    const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
     setMovies(data.Search)
@@ -41,8 +41,8 @@ const App = () => {
         <div className='search'>
           <input 
             placeholder='Search for movies'
-            value='Superman'
-            onChange={() => {}}
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <img 
             src={SearchIcon}
@@ -51,21 +51,17 @@ const App = () => {
           />
         </div>
 
-        {
-          movies?.length > 0
-          ?  (
-            <div className='container'>
-               {movies.map((movie) => 
-                 <MovieCard movie={movie}/>
-               )}
-             </div>
-             )
-          : (
-            <div className='empty'>
-              <h2>No movies founded</h2>
-            </div>
-          )
-        }
+        {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
 
       </div>
     );
